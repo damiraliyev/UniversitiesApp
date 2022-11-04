18287c5d3379e9324c406485c316eed62e4fcf2e
damiraliyev <damir.aliyev.02@mail.ru>
2022-11-04 18:11:47 +0600

    add deleting functionality

--- UniversitiesApp/UniversitiesApp/MyUniversitiesViewController.swift
+++ UniversitiesApp/UniversitiesApp/MyUniversitiesViewController.swift
@@ -96,6 +96,18 @@ extension MyUniversitiesViewController: UITableViewDelegate {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            universities.remove(at: indexPath.row)
+            tableView.reloadData()
+            addedUnivers.remove(at: indexPath.row)
+        }
+    }
+    
 }
 
 
